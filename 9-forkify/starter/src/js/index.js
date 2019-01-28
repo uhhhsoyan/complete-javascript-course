d84c98817235e92616b31ac790cc5ec617fac672
Eric Essoyan <eric.essoyan@gmail.com>
2019-01-28 20:26:22 +0300

    forkify project_2

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,11 @@
 // Global app controller
+
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = '9bf96871a08420ca49b5b2502b2e6f64';
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+}
+
+// 9bf96871a08420ca49b5b2502b2e6f64
+// https://www.food2fork.com/api/search
